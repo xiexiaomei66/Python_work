da7d68a12162a1d95536a14aa7bda633d0f2dd81
xiexiaomei66 <630906365@qq.com>
2019-05-17 17:14:46 +0800

    增加51自学网的代码

--- my_AutoTest/test_case/public.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from Driver import *
-class StartEnd(unittest.TestCase):
-    def setUp(self):
-        self.driver=browser()
-        self.driver.implicitly_wait(10)
-        self.driver.maximize_window()
-    def tearDown(self):
-        self.driver.quit()
